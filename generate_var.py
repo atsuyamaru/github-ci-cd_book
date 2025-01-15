0b1c43cef257c20505de5087757d68076ae3b376
atsuyamaru <atsuyamaru0816@gmail.com>
2025-01-15 20:17:45 +0900

    Removed deplicated syntax

--- generate_var.py
+++ generate_var.py
@@ -5,8 +5,8 @@ import os
 
 def generate_var(date: datetime.datetime, zone: ZoneInfo) -> str:
     value = date.astimezone(zone).strftime("%Y%m%d%H%M%S")
-    # Print special GitHub Actions syntax to set output
-    print(f"::set-output name=timestamp::{value}")
+    # # Print special GitHub Actions syntax to set output
+    # print(f"::set-output name=timestamp::{value}")
     # For newer GitHub Actions (>= 2022), you can also use this syntax:
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
         print(f"timestamp={value}", file=fh)
